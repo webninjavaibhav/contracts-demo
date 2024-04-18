555fa45ba2d5f2768a7eef66d8b786e92b6dedb3
vaibhavpatel <vaibhavP@agilebrainsconsulting.com>
2024-04-18 19:52:01 +0530

    .cjs file deleted

--- src/.puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-import { join } from "path";
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  // Changes the cache location for Puppeteer.
-  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
-};
